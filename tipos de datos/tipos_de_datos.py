baa7d26d4f98b2e21437a88e14086f67320b9434
victor <vrubinr501@gmail.com>
2025-07-22 17:09:26 +0200

    22/07/2025

---
+++ tipos de datos/tipos_de_datos.py
@@ -0,0 +1,21 @@
+"string"
+
+# se puede poner un string con saltos de linea si utilizas 3 comillas """"""
+
+texto = """string
+    hola mundo 
+    que tal"""
+
+print(texto)
+
+#numeros
+
+4
+4.4
+
+# boolean
+
+True 
+False
+
+
