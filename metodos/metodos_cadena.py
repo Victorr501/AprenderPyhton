2fe8ccbbe9e48ece5780bbcb627bad4291550a25
victor <vrubinr501@gmail.com>
2025-07-24 16:18:13 +0200

    24/07/2025

---
+++ metodos/metodos_cadena.py
@@ -0,0 +1,6 @@
+cadena1 = "hola soy victor"
+cadena2 = "esto es una prueba"
+
+resultado = cadena1.upper()
+
+print(resultado)
---
+++ operadores/op_logicos.py
@@ -0,0 +1,10 @@
+#AND
+Resultado = True and True
+
+
+#OR
+Resultado_or = False or False
+
+#NOT
+
+Resultado_not = not True 
