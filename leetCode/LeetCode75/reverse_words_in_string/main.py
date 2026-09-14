4dfaed44abc05647b9240edeabeb9a7b93346fcb
Víctor Rubín Rubio <vrubinr501@gmail.com>
2026-09-14 23:59:22 +0200

    Ejercicio 6 terminado pero quiero hacerlo de manera manual

--- leetCode/LeetCode75/reverse_words_in_string/main.py
+++ leetCode/LeetCode75/reverse_words_in_string/main.py
@@ -2,5 +2,5 @@ from reverse_words_in_string import Solution
 
 class main():
     my_solution = Solution()
-    resultado = my_solution.reverseVowels("leetcode")
+    resultado = my_solution.reverseWords("the sky is blue")
     print(resultado)
--- leetCode/LeetCode75/reverse_words_in_string/reverse_words_in_string.py
+++ leetCode/LeetCode75/reverse_words_in_string/reverse_words_in_string.py
@@ -1,6 +1,49 @@
 class Solution(object):
+    # def reverseWords(self, s):
+    #     """
+    #     :type s: str
+    #     :rtype: str
+    #     """
+
+    #     new_string = ""
+    #     guardar_palabra = []
+    #     ahi_palabra = False
+    #     anterior_es_espacio = False
+    #     palabra_que_es = 0
+        
+    #     for i in range(len(s) - 1, -1, -1):
+    #         if s[i] != " ":
+    #             if anterior_es_espacio:
+    #                 if not guardar_palabra:
+    #                     ahi_palabra = False
+                        
+    #             if ahi_palabra and palabra_que_es == 0 :
+    #                 palabra = "".join(guardar_palabra)
+    #                 new_string += palabra + " "
+    #                 guardar_palabra.clear()
+    #                 palabra_que_es += 1
+    #             elif ahi_palabra and palabra_que_es != 0:
+    #                 palabra = "".join(guardar_palabra)
+    #                 new_string += palabra
+    #                 guardar_palabra.clear()
+    #                 palabra_que_es += 1
+    #             guardar_palabra.insert(0, s[i])
+    #         elif s[i] == " ":
+    #             anterior_es_espacio = True
+                
+    #     return new_string
+
     def reverseWords(self, s):
         """
         :type s: str
         :rtype: str
         """
+        
+        palabras = s.split() # Corta la frase por los epacio automátcamente y elimina los sobrantes
+        return " ".join(reversed(palabras)) # Les da la buelta y " ".join() las vuelve a unir con un espacio
+        
+                
+        
+        
+        
+                
